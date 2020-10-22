30fb64865adbdda1c75c18a1528685857d01acb8
atemple <templeandrew@outlook.com>
2020-10-22 11:44:01 -0400

    Start setting up the main module

--- index.js
+++ index.js
@@ -1,5 +1,17 @@
+const apiClient = require('./src/clients/api-client');
+const merchantMiddleware = require('./src/middleware/merchant-middleware');
+const hashingUtility = require('./src/utility/hashing-utility');
 
 const reqhub = {
+  apiClient: {
+    ...apiClient
+  },
+  middleware: {
+    ...merchantMiddleware
+  },
+  utilities: {
+    ...hashingUtility
+  }
 };
 
 exports.reqhub = reqhub;
