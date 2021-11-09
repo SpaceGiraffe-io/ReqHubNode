ed01c35e3541008d449d983ed7d38f83f0c659a0
atemple <templeandrew@outlook.com>
2021-11-09 12:11:55 -0500

    Include additional utilities and fix client URLs

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const apiClient = require('./src/clients/api-client');
 const merchantMiddleware = require('./src/middleware/merchant-middleware');
 const hashingUtility = require('./src/utility/hashing-utility');
+const httpUtility = require('./src/utility/http-utility');
+const reqhubUtility = require('./src/utility/reqhub-utility');
 
 exports.apiClient = {
   ...apiClient
@@ -11,6 +13,8 @@ exports.middleware = {
 };
 
 exports.utilities = {
-  ...hashingUtility
+  ...hashingUtility,
+  ...httpUtility,
+  ...reqhubUtility
 };
 
